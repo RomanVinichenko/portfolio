feffd6c8a3d5009b623fa0e452f513a4507bb46d
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-30 23:40:24 +0300

    Updates

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.portfolio__list', {
+  loop: true,
+  speed: 1000,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+$('.menu a, .intro a, .nav__items a').on('click', function (event) {
+  event.preventDefault();
+  var id = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate(
+    {
+      scrollTop: top,
+    },
+    1100,
+  );
+});
